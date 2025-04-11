107596d49c59dd377b286a8335c08b8dd6971b84
youyo <1003ni2@gmail.com>
2025-04-11 12:08:28 +0900

    fix: adjust project scripts and package directory configuration

--- src/slacraper/_version.py
+++ src/slacraper/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev0+d20250411'
-__version_tuple__ = version_tuple = (0, 1, 'dev0', 'd20250411')
+__version__ = version = '0.1.1.dev0+g3873ab2.d20250411'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev0', 'g3873ab2.d20250411')
